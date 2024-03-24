031002a757581380406b600ceb3eff910dbae000
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-03-24 16:58:54 +0400

    fix flag

--- go_src_gen.sh
+++ go_src_gen.sh
@@ -6,7 +6,7 @@ sudo apt install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
-protoc --go_out./go --go_opt=paths=source_relative \
+protoc --go_out=./go --go_opt=paths=source_relative \
     --go-grpc_out=./go --go-grpc_opt=paths=source_relative \
     ./${SERVICE_NAME}/*.proto
 
