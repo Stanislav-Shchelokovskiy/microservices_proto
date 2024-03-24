daa2606c875837c7c476de4109f6731e26a7f184
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-03-24 17:29:36 +0400

    fix gen commands

--- go_src_gen.sh
+++ go_src_gen.sh
@@ -2,20 +2,27 @@
 SERVICE_NAME=$1
 RELEASE_VERSION=$2
 
+echo "Install deps"
 sudo apt install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+echo "Ensure go dir"
+mkdir -p go/${SERVICE_NAME}
+
+echo "Compile protos"
 protoc --go_out=./go --go_opt=paths=source_relative \
     --go-grpc_out=./go --go-grpc_opt=paths=source_relative \
     ./${SERVICE_NAME}/*.proto
 
+echo "Init mod"
 cd go/${SERVICE_NAME}
 go mod init \ 
     git@github.com:Stanislav-Shchelokovskiy/microservices_proto/go/${SERVICE_NAME} ||true
 go mod tidy
 cd ../..
 
+echo "Git push"
 git config --global user.email stanislav.shchelokovskiy@gmail.com
 git config --global user.name Stanislav Shchelokovskiy
 git add . && git commit -am "proto update" ||true
